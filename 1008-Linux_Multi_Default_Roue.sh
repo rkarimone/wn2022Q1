643d03d6111149bd5f1c321cc6b4d6044f9ec94a
REZAUL KARIM <110754378+rkarimone@users.noreply.github.com>
2024-03-13 19:29:25 +0600

    Update 1008-Linux_Multi_Default_Roue.sh

--- 1008-Linux_Multi_Default_Roue.sh
+++ 1008-Linux_Multi_Default_Roue.sh
@@ -55,5 +55,65 @@ wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10/amd64/linux-modules
 sudo dpkg -i *.deb
 
 
+root@vpnserver:~# cat /etc/iproute2/rt_tables
+#
+# reserved values
+#
+255	local
+254	main
+253	default
+0	unspec
+#
+# local
+#
+#1	inr.ruhep
+1 enp1s0f1
+2 enp1s0f2
+
+
+
+root@vpnserver:~# cat /opt/link3_up.sh 
+#!/bin/bash
+#route del default
+#ip route add default via 123.200.2.33
+
+ip route add 123.200.2.32/29 dev enp1s0f2 src 123.200.2.36 table enp1s0f2
+ip route add default via 123.200.2.33 dev enp1s0f2 table enp1s0f2
+ip rule add from 123.200.2.36/32 table enp1s0f2
+ip rule add to 123.200.2.36/32 table enp1s0f2
+#
+sleep 5
+ethtool -K enp1s0f0 tso off
+ethtool -K enp1s0f0 sg off
+ethtool -K enp1s0f1 tso off
+ethtool -K enp1s0f1 sg off
+ethtool -K enp1s0f2 tso off
+ethtool -K enp1s0f2 sg off
+ethtool -K enp1s0f3 tso off
+ethtool -K enp1s0f3 sg off
+
+sleep 1
+/etc/init.d/vpnserver restart >/dev/null 2>&1
+
+root@vpnserver:~# cat /opt/nexto_up.sh 
+#!/bin/bash
+route del default
+ip route add default via 103.230.62.65
+
+#
+sleep 5
+ethtool -K enp1s0f0 tso off
+ethtool -K enp1s0f0 sg off
+ethtool -K enp1s0f1 tso off
+ethtool -K enp1s0f1 sg off
+ethtool -K enp1s0f2 tso off
+ethtool -K enp1s0f2 sg off
+ethtool -K enp1s0f3 tso off
+ethtool -K enp1s0f3 sg off
+
+sleep 1
+/etc/init.d/vpnserver restart >/dev/null 2>&1
+
+
 
 
